51c7ffec7952695c482e86ddeadc545aa23b2a91
wujia777 <cwu005104@gmail.com>
2022-06-06 22:12:51 +0800

    feat: initialize index page

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
+//const restaurantList = require('')
 
 //載入express功能
 const express = require('express')
 const app = express()
 const port = 3000
 
-//呼叫樣板引擎express-handlebars
+//呼叫樣板引擎express-hbs
 const exphbs = require('express-handlebars')
-
-
 
 //呼叫Mongoose資料庫
 const mongoose = require('mongoose')
@@ -30,17 +28,17 @@ db.once('open', () => {
 })
 
 //設定模板引擎
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 //設定靜態檔案
 app.use(express.static('public'))
 
 //設定路由根目錄
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results })
+    res.render('index')
 })
-app.get('/restaurants/:restaurantId', (req, res) => {
+/*app.get('/restaurants/:restaurantId', (req, res) => {
     const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurantId)
     res.render('show', { restaurant: restaurant })
 })
@@ -51,7 +49,7 @@ app.get('/search', (req, res) => {
         return restaurant.name.toLowerCase().includes(keyword)
     })
     res.render('index', { restaurants: restaurants, keyword: keyword })
-})
+})*/
 //接收server
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
--- models/restaurant.js
+++ models/restaurant.js
@@ -10,30 +10,39 @@ const restaurantSchema = new Schema({
     },
     name: {
         type: String,
+        required: true
     },
     name_en: {
         type: String,
+        required: true
     },
     category: {
         type: String,
+        required: true
     },
     image: {
         type: String,
+        required: true
     },
     location: {
         type: String,
+        required: true
     },
     phone: {
         type: String,
+        required: true
     },
     google_map: {
         type: String,
+        required: true
     },
     rating: {
         type: Number,
+        required: true
     },
     description: {
         type: String,
+        required: true
     },
 })
 
