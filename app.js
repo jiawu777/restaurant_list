46470d3e926ae8f6a14da12ebc42517bd53c0148
wujia777 <cwu005104@gmail.com>
2022-07-24 19:50:24 +0800

    feat: rewrite A1-add search bar trim & simplified

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 //載入Restaurant Model
-const Restaurant = require('./models/restaurant')
-
-
+const restaurantList = require('./restaurant.json').results
 
 //載入express功能
 const express = require('express')
@@ -16,12 +14,6 @@ const port = 3000
 
 //呼叫樣板引擎express-hbs
 const exphbs = require('express-handlebars')
-
-//取用資料庫
-const restaurant = require('./models/restaurant')
-
-//載入mongoose
-require('./config/mongoose')
 
 //設定模板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -33,78 +25,34 @@ app.use(express.static('public'))
 //用app.use規定每筆請求都需要透過body-parser進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//用app.use先跑 method override(function) 跑路由，只要網址帶有_method參數，就可以使用http方法(可用POST跟DELETE)呼叫
-app.use(methodOverride('_method'))
-
-//新增new頁面
-app.get('/restaurants/new', (req, res) => {
-    res.render('new')
-})
-
-//新增一筆餐廳資料
-app.post('/restaurants', (req, res) => {
-    console.log(req.body)
-    Restaurant.create(req.body)
-        .then(() => res.redirect('/'))
-        .catch(error => console.error(error))
-
+//顯示首頁
+app.get('/', (req, res) => {
+    res.render('index', { restaurants: restaurantList })
 })
 
 //瀏覽特定資料
-app.get('/restaurants/:id', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .lean()
-        .then((restaurant) => res.render('detail', { restaurant }))
-        .catch(error => console.error(error))
+app.get('/restaurants/:restaurantId', (req, res) => {
+    const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurantId)
+    res.render('detail', { restaurant: restaurant })
 
 })
 
-//修改特定資料
-
-//進入edit頁面
-app.get('/restaurants/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .lean()
-        .then((restaurant) => res.render('edit', { restaurant }))
-        .catch(error => console.error(error))
-
-})
-//上傳修改資料
-app.post('/restaurants/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findByIdAndUpdate(id, req.body)
-        .then((restaurant) => res.redirect(`/restaurants/${id}`))
-        .catch(error => console.error(error))
-})
-
-//刪除資料
-app.post('/restaurants/:id/delete', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .then(restaurant => restaurant.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.error(error))
-})
-/*
-app.post('/restaurants/:id/edit', (req, res) => {
-    const editItem = req.body
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .then(editItem =>
-            restaurant.editItem)
-})*/
-
-/*
 //search功能get
 app.get('/search', (req, res) => {
-    const keyword = req.query.keyword.toLowerCase()
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword)
-    })
-    res.render('index', { restaurants: restaurants, keyword: keyword })
-})*/
+
+    const keyword = req.query.keyword.toLowerCase().trim()
+    const filteredRestaurants = restaurantList.filter(item => item.name.toLowerCase().includes(keyword) || item.category.includes(keyword))
+
+    /*//搜尋結果無法跳出彈出視窗，問題點在這
+    if (!filteredRestaurants.length) {
+        const noSearchResult = `查無相關結果`
+        res.render('index', { alert: noSearchResult, restaurants: restaurantList, keyword: keyword })
+    } else {
+        res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
+    }*/
+    res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
+})
+
 //接收server
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
