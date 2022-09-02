7a66978e09ced8da59c47910772f99f1db5041fd
wujia777 <cwu005104@gmail.com>
2022-09-02 23:20:55 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')//呼叫樣板引擎express-hbs
 const bodyParser = require('body-parser')//載入body-parse把URL-encoded轉譯成req.body物件
 const methodOverride = require('method-override')//載入method override
+const session = require('express-session')
 
 const routes = require('./routes')//預設直接找index可省略
 require('./config/mongoose')//載入mongoose
@@ -11,6 +12,11 @@ const app = express()
 
 app.use(express.static('public'))//設定靜態檔案(未更動)
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))//設定模板引擎
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))//用app.use規定每筆請求都需要透過body-parser進行前置處理
 app.use(methodOverride('_method'))//用methodOverride把程式碼Restful化
